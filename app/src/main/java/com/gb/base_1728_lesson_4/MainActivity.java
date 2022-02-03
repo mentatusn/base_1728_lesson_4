2edcdbba5ca70a0d18e7dfb918911f0eaec561c4
Andej Nesterenko <mentatus.n@gmail.com>
2022-02-03 21:08:40 +0300

    урок 5 научились работать с startActivityForResult и setResult

--- app/src/main/java/com/gb/base_1728_lesson_4/MainActivity.java
+++ app/src/main/java/com/gb/base_1728_lesson_4/MainActivity.java
@@ -1,33 +1,44 @@
 package com.gb.base_1728_lesson_4;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  {
 
 
-    public static String KEY_INTENT = "key1";
+    public static String KEY_INTENT_FROM_MAIN_TO_SECOND = "key1235";
+    public static String KEY_INTENT_FROM_SECOND_TO_MAIN = "key2346";
+    public static int REQUEST_CODE = 999;
     Button btn;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
+        tv = findViewById(R.id.tv);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-                intent.putExtra(KEY_INTENT,"Привет, SecondActivity! Я из MainActivity");
-                startActivity(intent);
+                intent.putExtra(KEY_INTENT_FROM_MAIN_TO_SECOND,"Привет, SecondActivity! Я из MainActivity");
+                startActivityForResult(intent,REQUEST_CODE);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_CODE&&resultCode==RESULT_OK){
+            if(data.getExtras()!=null)
+            tv.setText(data.getStringExtra(KEY_INTENT_FROM_SECOND_TO_MAIN));
+        }
+    }
 }
--- app/src/main/java/com/gb/base_1728_lesson_4/SecondActivity.java
+++ app/src/main/java/com/gb/base_1728_lesson_4/SecondActivity.java
@@ -2,23 +2,45 @@ package com.gb.base_1728_lesson_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView tv;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         tv = findViewById(R.id.tv);
+        btn = findViewById(R.id.btn);
 
         Bundle extras = getIntent().getExtras();
         if(extras!=null){
-            tv.setText(extras.getString(MainActivity.KEY_INTENT));
+            tv.setText(extras.getString(MainActivity.KEY_INTENT_FROM_MAIN_TO_SECOND));
         }
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra(MainActivity.KEY_INTENT_FROM_SECOND_TO_MAIN,"Привет, MainActivity! У меня все хорошо, сама как?");
+                SecondActivity.this.setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
