9e43120fb92604f4b165f22ac89554b179c84cd8
Andej Nesterenko <mentatus.n@gmail.com>
2022-02-14 22:30:28 +0300

    урок 8 добавили drawer(факультативно)

--- app/src/main/java/com/gb/base_1728_lesson_6/MyFragmentsActivity.java
+++ app/src/main/java/com/gb/base_1728_lesson_6/MyFragmentsActivity.java
@@ -5,18 +5,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.gb.base_1728_lesson_4.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class MyFragmentsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_fragments);
+        setContentView(R.layout.bonus_activity_my_fragments);
 
         if (savedInstanceState == null) {
             CitiesFragment citiesFragment = CitiesFragment.newInstance();
@@ -25,6 +28,31 @@ public class MyFragmentsActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.burger_open,R.string.burger_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case (R.id.action_drawer_about):{
+                        getSupportFragmentManager().beginTransaction().replace(R.id.cities,new AboutFragment()).addToBackStack("").commit();
+                        drawerLayout.close();
+                        return true;
+                    }
+                    case (R.id.action_drawer_exit):{
+                        finish();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
     }
 
     @Override
