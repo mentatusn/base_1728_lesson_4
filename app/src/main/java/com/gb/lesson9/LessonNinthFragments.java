af0b7b139a8f8a368d744bb732d4432c8587baad
Andej Nesterenko <mentatus.n@gmail.com>
2022-02-17 21:14:32 +0300

    урок 9. Познакомились с AlertDialog+custom

--- app/src/main/java/com/gb/lesson9/LessonNinthFragments.java
+++ app/src/main/java/com/gb/lesson9/LessonNinthFragments.java
@@ -1,9 +1,12 @@
 package com.gb.lesson9;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -33,7 +36,8 @@ public class LessonNinthFragments extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
     }
-    void initView(View view){
+
+    void initView(View view) {
         /*view.findViewById(R.id.btnToast).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,19 +47,57 @@ public class LessonNinthFragments extends Fragment {
         view.findViewById(R.id.btnToast).setOnClickListener(v -> showToast());
         view.findViewById(R.id.btnSnackBar).setOnClickListener(v -> showSnackBar(view));
         view.findViewById(R.id.btnSnackBarWithAction).setOnClickListener(v -> showSnackBarWithAction(view));
+        view.findViewById(R.id.btnAlertDialog).setOnClickListener(v -> showAlertDialog());
+        view.findViewById(R.id.btnAlertDialogCustom).setOnClickListener(v -> showAlertDialogCustom());
     }
 
-    void showSnackBarWithAction(View view){
-        Snackbar.make(view,"SnackBarWithAction работает",Snackbar.LENGTH_LONG).setAction(
+    void showAlertDialogCustom() {
+        View view = getLayoutInflater().inflate(R.layout.dialog_custom, null);
+        AlertDialog alertDialog = new AlertDialog.Builder(requireContext())
+                .setTitle("AlertDialog")
+                .setMessage("AlertDialog message")
+                .setView(view)
+                .show();
+        view.findViewById(R.id.buttonCustomView).setOnClickListener(v -> {
+            EditText editText = view.findViewById(R.id.editTextCustomView);
+            showToast(editText.getText().toString());
+            alertDialog.dismiss();
+        });
+    }
+
+    void showAlertDialog() {
+        new AlertDialog.Builder(requireContext())
+                .setTitle("AlertDialog")
+                .setMessage("AlertDialog message")
+                .setPositiveButton("Да", (dialog, which) -> {
+                    showToast("Да");
+                })
+                .setNegativeButton("Нет", (dialog, which) -> {
+                    showToast("Нет");
+                })
+                .setNeutralButton("Ну не знаю", (dialog, which) -> {
+                    showToast("Ну не знаю");
+                })
+                .show();
+    }
+
+    void showSnackBarWithAction(View view) {
+        Snackbar.make(view, "SnackBarWithAction работает", Snackbar.LENGTH_LONG).setAction(
                 "Попробововать еще раз", v -> {
                     showToast();
                 }
         ).show(); //TODO
     }
-    void showSnackBar(View view){
-        Snackbar.make(view,"SnackBar работает",Snackbar.LENGTH_LONG).show();
+
+    void showSnackBar(View view) {
+        Snackbar.make(view, "SnackBar работает", Snackbar.LENGTH_LONG).show();
     }
-    void showToast(){
-        Toast.makeText(requireContext(),"Toast работает",Toast.LENGTH_LONG).show();
+
+    void showToast() {
+        Toast.makeText(requireContext(), "Toast работает", Toast.LENGTH_LONG).show();
+    }
+
+    void showToast(String message) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show();
     }
 }
